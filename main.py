746ecf29265cf3e07b98f3a514ab845cbad35c12
Liu Yixi <39328006+Kylecc97@users.noreply.github.com>
2018-06-04 16:20:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+from PyQt5 import QtCore, QtWidgets
+import sys
+sys.path.append("/users/lyx/Documents/face_recognition/background/face_recognition/GUI")
 import mainwindow
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    ui = mainwindow.MainWindow()
+    ui.show()
+    sys.exit(app.exec_())
+
